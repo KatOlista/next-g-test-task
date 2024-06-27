bde2ba75a380dd3029f02d6ddbdad8c7c63ac690
KatOlista^^! <olya.katyushina@gmail.com>
2024-06-27 18:18:50 +0200

    test-task

---
+++ app/js/showMenu.js
@@ -0,0 +1,22 @@
+const menu = document.getElementsByClassName('header__menu')[0];
+const page = document.getElementsByClassName('page')[0];
+
+const showMenuHandler = () => {
+  menu.classList.remove('animate__slideOutUp');
+  menu.classList.add('visible', 'animate__animated', 'animate__slideInDown');
+  page.classList.add('lock');
+};
+
+const hideMenuHandler = () => {
+  menu.classList.remove('animate__slideInDown');
+  menu.classList.add('animate__slideOutUp');
+  page.classList.remove('lock');
+
+  setTimeout(() => {
+    menu.classList.remove('visible'); 
+  }, 1500)
+};
+
+document.getElementsByClassName('header__menubtn')[0].addEventListener('click', showMenuHandler);
+
+document.getElementsByClassName('header__close')[0].addEventListener('click', hideMenuHandler);
